2cf73f61a0068109cf313052bbae9a2c59802397
Matt Butterfield <matt.butterfield@percolate.com>
2015-03-15 13:42:41 -0400

    add pan and swipe

--- swiftris/GameViewController.swift
+++ swiftris/GameViewController.swift
@@ -13,6 +13,7 @@ class GameViewController: UIViewController, SwiftrisDelegate, UIGestureRecognize
 
     var scene: GameScene!
     var swiftris:Swiftris!
+    var panPointReference:CGPoint?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,6 +44,44 @@ class GameViewController: UIViewController, SwiftrisDelegate, UIGestureRecognize
         swiftris.rotateShape()
     }
 
+    @IBAction func didPan(sender: UIPanGestureRecognizer) {
+        let currentPoint = sender.translationInView(self.view)
+        if let originalPoint = panPointReference {
+            if abs(currentPoint.x - originalPoint.x) > (BlockSize * 0.9) {
+                if sender.velocityInView(self.view).x > CGFloat(0) {
+                    swiftris.moveShapeRight()
+                    panPointReference = currentPoint
+                } else {
+                    swiftris.moveShapeLeft()
+                    panPointReference = currentPoint
+                }
+            }
+        } else if sender.state == .Began {
+            panPointReference = currentPoint
+        }
+    }
+
+    @IBAction func didSwipe(sender: UISwipeGestureRecognizer) {
+        swiftris.dropShape()
+    }
+
+    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer!, shouldRecognizeSimultaneouslyWithGestureRecognizer otherGestureRecognizer: UIGestureRecognizer!) -> Bool {
+        return true
+    }
+
+    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer!, shouldBeRequiredToFailByGestureRecognizer otherGestureRecognizer: UIGestureRecognizer!) -> Bool {
+        if let swipeRec = gestureRecognizer as? UISwipeGestureRecognizer {
+            if let panRec = otherGestureRecognizer as? UIPanGestureRecognizer {
+                return true
+            }
+        } else if let panRec = gestureRecognizer as? UIPanGestureRecognizer {
+            if let tapRec = otherGestureRecognizer as? UITapGestureRecognizer {
+                return true
+            }
+        }
+        return false
+    }
+
     func didTick() {
         swiftris.letShapeFall()
     }
@@ -52,7 +91,6 @@ class GameViewController: UIViewController, SwiftrisDelegate, UIGestureRecognize
         if let fallingShape = newShapes.fallingShape {
             self.scene.addPreviewShapeToScene(newShapes.nextShape!) {}
             self.scene.movePreviewShape(fallingShape) {
-                // #2
                 self.view.userInteractionEnabled = true
                 self.scene.startTicking()
             }
@@ -80,7 +118,10 @@ class GameViewController: UIViewController, SwiftrisDelegate, UIGestureRecognize
     }
 
     func gameShapeDidDrop(swiftris: Swiftris) {
-
+        scene.stopTicking()
+        scene.redrawShape(swiftris.fallingShape!) {
+            swiftris.letShapeFall()
+        }
     }
 
     func gameShapeDidLand(swiftris: Swiftris) {
